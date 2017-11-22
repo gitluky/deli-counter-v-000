a32f646e7e9af5cda122147b8cf88182db97c29c
Jonathan Luk <jluk87@gmail.com>
2017-11-22 04:10:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length < 1
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{array.each {|person| " #{array.index(person)+1}. #{person}"}
+  end
+      }"
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+end
+
+array = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+line(array)
